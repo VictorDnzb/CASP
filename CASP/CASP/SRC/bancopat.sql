1e2f6688be47714593d3c082c197397fe0f3f1f5
Enzo-Dan <132352773+Enzo-Dan@users.noreply.github.com>
2025-10-27 14:52:57 -0300

    Update bancopat.sql

--- CASP/CASP/SRC/bancopat.sql
+++ CASP/CASP/SRC/bancopat.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE patrimonio;
+
 USE patrimonio;
 
 CREATE TABLE patrimonio (
@@ -9,18 +10,28 @@ CREATE TABLE patrimonio (
     condicao VARCHAR(50) NOT NULL,
     origem VARCHAR(50) NOT NULL,
     marca VARCHAR(100),
-    codigo_doador VARCHAR(100),
-    codigo_cps VARCHAR(100),
+    codigo_doador VARCHAR(7),
+    codigo_cps VARCHAR(7),
     quantidade INT DEFAULT 1,
     imagem VARCHAR(255),
     data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
     usuario_cadastro VARCHAR(100)
 );
 
-CREATE TABLE users (
+
+ALTER TABLE patrimonio 
+ADD COLUMN data_atualizacao DATETIME NULL AFTER data_cadastro,
+ADD COLUMN usuario_atualizacao VARCHAR(100) NULL AFTER usuario_cadastro;
+
+delete from patrimonio where localizacao = "ESTEVAM FERRI";
+
+
+CREATE TABLE IF NOT EXISTS usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    ativo BOOLEAN DEFAULT TRUE,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-select * from patrimonio;
+show tables;
